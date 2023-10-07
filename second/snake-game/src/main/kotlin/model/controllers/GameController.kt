f0f8e7ed3610d1b683524a4a475ac2357262b62c
ykhdr <yanhudorozhkov@gmail.com>
2023-10-07 17:15:45 +0700

    [BROKEN] Added GameController, updated ProtoMapper

---
+++ second/snake-game/src/main/kotlin/model/controllers/GameController.kt
@@ -0,0 +1,4 @@
+package model.controllers
+
+class GameController {
+}
---
+++ second/snake-game/src/main/kotlin/model/mappers/ProtoMapper.kt
@@ -0,0 +1,13 @@
+package model.mappers
+
+import me.ippolitov.fit.snakes.SnakesProto
+import model.dto.messages.GameMessage
+
+/**
+ * Маппер из обьектов бизнес логики в Proto
+ */
+object ProtoMapper{
+    fun toMessage(protoMessage: SnakesProto.GameMessage) : GameMessage{
+
+    }
+}
