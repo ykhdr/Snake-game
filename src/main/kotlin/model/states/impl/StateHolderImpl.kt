f23dc65f442a92ba23c539f1025c3a0cb18deb78
ykhdr <yanhudorozhkov@gmail.com>
2023-11-20 13:52:51 +0700

    Added cached state in stateHolder

--- src/main/kotlin/model/states/impl/StateHolderImpl.kt
+++ src/main/kotlin/model/states/impl/StateHolderImpl.kt
@@ -9,35 +9,38 @@ import model.states.StateHolder
 
 class StateHolderImpl : StateHolder {
     private val stateEditor = StateEditorImpl()
+    private lateinit var cachedState : State
+
+    init {
+        getState()
+    }
 
     override fun isNodeMaster(): Boolean {
-        val state = stateEditor.edit()
-        return state.getNodeRole() == NodeRole.MASTER
+        return cachedState.getNodeRole() == NodeRole.MASTER
     }
 
     override fun getGameAnnouncement(): GameAnnouncement {
-        val state = stateEditor.edit()
 
         return GameAnnouncement(
-            state.getPlayers(),
-            state.getConfig(),
-            state.getAvailableCoords().isNotEmpty(),
-            state.getGameName()
+            cachedState.getPlayers(),
+            cachedState.getConfig(),
+            cachedState.getAvailableCoords().isNotEmpty(),
+            cachedState.getGameName()
         )
     }
 
     override fun getState(): State {
-        return stateEditor.edit()
+        cachedState = stateEditor.edit()
+        return cachedState
     }
 
     override fun getStateEditor(): StateEditor = stateEditor
     override fun getGameState(): GameState {
-        val state = stateEditor.edit()
         return GameState(
-            state.getStateOrder(),
-            state.getSnakes(),
-            state.getFoods(),
-            state.getPlayers(),
+            cachedState.getStateOrder(),
+            cachedState.getSnakes(),
+            cachedState.getFoods(),
+            cachedState.getPlayers(),
         )
     }
 }
