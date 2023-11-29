8f846aebfd974122c497839b4b3157909efc07c1
ykhdr <yanhudorozhkov@gmail.com>
2023-11-29 17:35:08 +0700

    Removed redundant field

--- src/main/kotlin/model/api/config/NetworkConfig.kt
+++ src/main/kotlin/model/api/config/NetworkConfig.kt
@@ -6,13 +6,11 @@ import java.net.NetworkInterface
 
 data class NetworkConfig(
     val groupAddress: InetSocketAddress = groupAddress(),
-    val localAddress: InetSocketAddress = localAddress(),
     val nodeSocket: DatagramSocket = nodeSocket(),
     val localInterface: NetworkInterface = localInterface()
 ) {
     companion object Defaults {
         private fun groupAddress(): InetSocketAddress = InetSocketAddress("239.192.0.4", 9192)
-        private fun localAddress() : InetSocketAddress = InetSocketAddress("127.0.0.1",1246)
         private fun nodeSocket(): DatagramSocket = DatagramSocket()
         private fun localInterface(): NetworkInterface = NetworkInterface.getByName("wlp3s0")
     }
