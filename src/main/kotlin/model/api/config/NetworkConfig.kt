128d41dd2639b3e84bd0f870626b88f4d68491c0
ykhdr <yanhudorozhkov@gmail.com>
2023-11-29 13:22:46 +0700

    Updated NetworkConfig

--- src/main/kotlin/model/api/config/NetworkConfig.kt
+++ src/main/kotlin/model/api/config/NetworkConfig.kt
@@ -5,10 +5,12 @@ import java.net.NetworkInterface
 
 data class NetworkConfig(
     val groupAddress: InetSocketAddress = groupAddress(),
+    val localAddress: InetSocketAddress = localAddress(),
     val localInterface: NetworkInterface = localInterface()
 ) {
     companion object Defaults {
         private fun groupAddress(): InetSocketAddress = InetSocketAddress("239.192.0.4", 9192)
-        private fun localInterface(): NetworkInterface = NetworkInterface.getByName("wlp3s0")
+        private fun localAddress() : InetSocketAddress = InetSocketAddress(1244)
+        private fun localInterface(): NetworkInterface = NetworkInterface.getByName("en0")
     }
 }
